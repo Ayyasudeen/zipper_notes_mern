9ef9ae5f0ec5dcb12c66a2bafb630b37fc835258
ayyasudeen <ayyasudeen@gmail.com>
2022-08-25 22:17:11 +0530

    Get method
    Using Data.js to make an API endpoint to fetch a json data
    
    Hitting localhost:5000/api/notes returns a json data from the backend

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const notes = require("./data/notes");
 
 const app = express();
 
@@ -6,4 +7,8 @@ app.listen(5000, console.log("Server Started on PORT 5000"));
 
 app.get("/", (req, res) => {
   res.send("API is running...");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
 });
