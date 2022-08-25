130fa51f610c405081e24649734a7f588cbf121a
ayyasudeen <ayyasudeen@gmail.com>
2022-08-25 21:52:47 +0530

    Update server.js
    Creating API endpoint for testing
    Hitting localhost:5000 gives the response

--- backend/server.js
+++ backend/server.js
@@ -3,3 +3,7 @@ const express = require("express");
 const app = express();
 
 app.listen(5000, console.log("Server Started on PORT 5000"));
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
