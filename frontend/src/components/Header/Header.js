605c2a6785930073d6c1ba211a3e5375d865132f
ayyasudeen <ayyasudeen@gmail.com>
2022-08-31 20:16:51 +0530

    Header
    Take the Navbar component from react-bootstrap documentation and modify it

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,7 +1,46 @@
 import React from "react";
+import {
+  Container,
+  Nav,
+  Navbar,
+  NavDropdown,
+  Button,
+  Form,
+} from "react-bootstrap";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <Navbar bg="primary" expand="lg" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Notes Zipper</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="m-auto">
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+              </Form>
+            </Nav>
+            <Nav>
+              <Nav.Link href="/">My Notes</Nav.Link>
+              <NavDropdown title="Ayyas" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.2">
+                  My Profile
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">LogOut</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
 };
 
 export default Header;
