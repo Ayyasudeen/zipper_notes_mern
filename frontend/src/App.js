adafc21c53c377d11d4a8c57884333f864f8d3f6
ayyasudeen <ayyasudeen@gmail.com>
2022-09-02 22:20:42 +0530

    Adding React Router in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,16 @@ import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import LandingPage from "./screens/LandingPage/LandingPage";
+import { BrowserRouter, Route } from "react-router-dom";
 
 const App = () => (
-  <>
+  <BrowserRouter>
     <Header />
     <main>
-      <LandingPage />
+      <Route path="/" component={LandingPage} />
     </main>
     <Footer />
-  </>
+  </BrowserRouter>
 );
 
 export default App;
