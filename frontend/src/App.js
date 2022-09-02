ebf8b7f54a222fc055fd670828966abcfcf0e37b
ayyasudeen <ayyasudeen@gmail.com>
2022-09-02 22:41:22 +0530

    React Router New Version Code Change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,17 @@ import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import LandingPage from "./screens/LandingPage/LandingPage";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MyNotes from "./screens/MyNotes/MyNotes";
 
 const App = () => (
   <BrowserRouter>
     <Header />
     <main>
-      <Route path="/" component={LandingPage} exact />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/mynotes" element={<MyNotes />} />
+      </Routes>
     </main>
     <Footer />
   </BrowserRouter>
